ebf7ec5ccd662ace6916330b362fdd53a0a351fb
Lindsay Goins <lindsayggoins@gmail.com>
2021-07-01 15:40:11 -0600

    Added basic working navbar

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
 from flask import Flask, render_template
 import os
 
-# Configuration
 
 app = Flask(__name__)
 
-# Routes 
 
 @app.route('/')
 def root():
-    return render_template("layouts/base.html")
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 @app.route('/diys')
 def diys():
     return render_template("diys.html")
 
+@app.route('/art')
+def art():
+    return render_template("art.html")
 
-# Listener
+@app.route('/bugs')
+def bugs():
+    return render_template("bugs.html")
+
+@app.route('/fish')
+def fish():
+    return render_template("fish.html")
+
+@app.route('/seacreatures')
+def seacreatures():
+    return render_template("seacreatures.html")
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112)) 
-    #                                 ^^^^
-    #              You can replace this number with any valid port
-    
+    port = int(os.environ.get('PORT', 9112))     
     app.run(port=port, debug=True)
